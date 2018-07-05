686cd94bab07c7d9c1212f6d75deecf2f5000e34
PassionZale <372470745@qq.com>
2018-07-05 10:14:53 +0800

    logo 图片加入 webpack 打包

--- src/components/layouts/admin/TheHeader.vue
+++ src/components/layouts/admin/TheHeader.vue
@@ -3,7 +3,7 @@
       <Row>
         <Col span="12" :style="{textAlign:'left'}">
           <router-link :to="{ path: '/' }">
-            <img src="/src/assets/logo.png" style="width:auto;height:32px;vertical-align:middle;"/>
+            <img :src="logoIcon" style="width:auto;height:32px;vertical-align:middle;"/>
             <span style="color:#495060;margin-left: 10px;">{{ '欢迎你, ' + user.username }}</span>
           </router-link>
         </Col>
@@ -18,9 +18,15 @@
 
 <script>
 import { mapGetters } from "vuex";
+import logoIcon from "../../../assets/logo.png";
 export default {
   computed: {
     ...mapGetters(["user"])
+  },
+  data() {
+    return {
+      logoIcon
+    };
   },
   methods: {
     logout() {
